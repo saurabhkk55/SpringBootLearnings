73a3d8b6701c622551cfd5f3622cbbfa3deb4534
saurabh <saurabhkumarkardam25@gmail.com>
2026-04-04 14:44:36 +0530

    Stocks

--- streams/Stocks/Main.java
+++ streams/Stocks/Main.java
@@ -3,44 +3,6 @@ package streams.Stocks;
 import java.util.*;
 import java.util.stream.Collectors;
 
-//public class Main {
-//    public static void main(String[] args) {
-//        List<Stock> stockList = new ArrayList<>();
-//
-//        stockList.add(new Stock("buy", "infosys", 100));
-//        stockList.add(new Stock("buy", "infosys", 100));
-//        stockList.add(new Stock("sell", "infosys", 50));
-//        stockList.add(new Stock("sell", "infosys", 20));
-//
-//        stockList.add(new Stock("buy", "TCS", 20));
-//        stockList.add(new Stock("buy", "TCS", 40));
-//        stockList.add(new Stock("Sell", "TCS", 20));
-//
-//        stockList.add(new Stock("Sell", "Wipro", 40));
-//
-//        Map<String, List<Stock>> collect = stockList.stream()
-//                .collect(Collectors.groupingBy(stk -> stk.getStockName()));
-//
-//        // collect.forEach((x, y) -> System.out.println(x + " => " + y));
-//
-//        List<Map<String, Integer>> collect1 = collect.entrySet().stream()
-//                .map(stk -> {
-//                    String stockName = stk.getKey();
-//                    int net = 0;
-//                    for (Stock s : stk.getValue()) {
-//                        if (s.getType() == "buy") net += s.getQuantity();
-//                        else net -= s.getQuantity();
-//                    }
-//                    Map<String, Integer> StockAndItsNetQuantity = new HashMap<>();
-//                    StockAndItsNetQuantity.put(stockName, net);
-//                    return StockAndItsNetQuantity;
-//                })
-//                .collect(Collectors.toList());
-//
-//        collect1.forEach(x -> System.out.println(x));
-//    }
-//}
-
 public class Main {
     public static void main(String[] args) {
         List<Stock> stockList = new ArrayList<>();
@@ -56,9 +18,26 @@ public class Main {
 
         stockList.add(new Stock("Sell", "Wipro", 40));
 
+        // Approach 1
         Map<String, Integer> result = stockList.stream()
+                .collect(Collectors.groupingBy(Stock::getStockName, Collectors.collectingAndThen(
+                        Collectors.toList(),
+                        stocks -> {
+                            int netQuantity = 0;
+                            for (Stock st : stocks) {
+                                if (st.getType().equalsIgnoreCase("buy")) netQuantity += st.getQuantity();
+                                else netQuantity -= st.getQuantity();
+                            }
+                            return netQuantity;
+                        }
+                )));
+
+        result.forEach((k, v) -> System.out.println(k + " => " + v));
+
+        // Approach 1
+        Map<String, Integer> result1 = stockList.stream()
                 .collect(Collectors.groupingBy(
-                        streams.Stocks.Stock::getStockName,
+                        Stock::getStockName,
                         Collectors.collectingAndThen(
                                 Collectors.toList(),
                                 list -> list.stream()
@@ -68,7 +47,6 @@ public class Main {
                                         .sum()
                         )
                 ));
-
-        result.forEach((k, v) -> System.out.println(k + " => " + v));
+        result1.forEach((k, v) -> System.out.println(k + " => " + v));
     }
 }
